930e00463731e8aa6ba86b028d002a5a3c6da5ae
rocklessg <abdulhafiz.suleiman@decagon.dev>
2022-08-06 21:35:01 +0100

    Add project files.

---
+++ AlgorithmChallenge/ChallengeSolution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmChallenge
+{
+    public static class ChallengeSolution
+    {
+        public static List<int> TwoNumbersFromAnArraySumUptoEqualTargetNumber(List<int> array, int target)
+        {
+            List<int> result = new List<int>();
+
+            for (int i = 0; i < array.Count; i++)
+            {
+                var targetCheck = target - array[i];
+
+                if (array.Contains(targetCheck) && targetCheck + array[i] == target)
+                {
+                    result.Add(array[i]);
+                    result.Add(targetCheck);
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ AlgorithmChallenge/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AlgorithmChallenge;
+
+Console.WriteLine("Hello, Supervisor!");
+
+var result = ChallengeSolution.TwoNumbersFromAnArraySumUptoEqualTargetNumber(new List<int> { 15, 11, 7, 2, 9 }, 9);
+foreach (var item in result)
+{
+    Console.Write(item + " ");
+}
