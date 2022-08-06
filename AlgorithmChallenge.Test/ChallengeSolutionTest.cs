e6ce52f064d8df36b962b4b306de6e05c324eee6
rocklessg <abdulhafiz.suleiman@decagon.dev>
2022-08-06 23:25:15 +0100

    Add xUnit test

--- AlgorithmChallenge.Test/ChallengeSolutionTest.cs
+++ AlgorithmChallenge.Test/ChallengeSolutionTest.cs
@@ -11,6 +11,8 @@ namespace AlgorithmChallenge.Test
         [Theory]
         [InlineData(new int[] { 2, 7, 11, 15 }, 9, new int[] { 2, 7 })]
         [InlineData(new int[] { 16, 10, 7, 2 }, 9, new int[] { 7, 2 })]
+        [InlineData(new int[] { 1, 10, 7, 5 }, 6, new int[] { 1, 5 })]
+        [InlineData(new int[] { 10, 5, 7, 1 }, 6, new int[] { 5, 1 })]
         public void ChallengeSolution_ValidateAlgorithmResult_ReturnArrayOfExpectedNumbers(int[] array, int target, int[] result)
         {
             var act = ChallengeSolution.TwoNumbersFromAnArraySumUptoEqualTargetNumber(array.ToList(), target);
