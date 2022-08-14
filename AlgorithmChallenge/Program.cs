a3f4bdf7725f1fbc72ed3be4bea55b6cb1718b71
rocklessg <abdulhafiz.suleiman@decagon.dev>
2022-08-14 13:51:11 +0100

    File System Demo

--- AlgorithmChallenge/Program.cs
+++ AlgorithmChallenge/Program.cs
@@ -1,12 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using AlgorithmChallenge;
+using FileSystem;
 
-Console.WriteLine("Hello!");
+//Console.WriteLine("Hello!");
 
 var result = ChallengeSolution.TwoNumbersFromAnArraySumUptoEqualTargetNumber(new List<int> { 2, 7, 11, 15 }, 9);
 foreach (var item in result)
 {
     Console.Write(item + " ");
 }
+Console.WriteLine("\n");
+
+
+FileInputOutput.ReadFromFile();
 
 
---
+++ FileSystem/FileInputOutput.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSystem
+{
+    public static class FileInputOutput
+    {
+        public static void ReadFromFile()
+        {
+            StreamWriter writer = new("test.txt");
+            writer.WriteLine("I see no changes \n wake up in the morning and I ask myself \n is life worth living should I blast myself");
+            writer.Close();
+
+            StreamReader reader = new("Test.txt");
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+            string line = reader.ReadToEnd();
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+            Console.WriteLine(line);
+            reader.Close();
+        }
+    }
+}
